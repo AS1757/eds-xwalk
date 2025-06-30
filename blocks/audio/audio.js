591be410c7ae52fa71ae4c02df3a160fa217816d
Amit K Singh <Amit.Kumar.Singh8@gds.ey.com>
2025-06-30 11:21:42 +0530

    restructure component models definitions and filters

--- blocks/audio/audio.js
+++ blocks/audio/audio.js
@@ -1,5 +1,9 @@
 export default function decorate(block) {
   const audioContainer = block.children[1];
-  const audioSource = block.querySelector("a").href;
-  audioContainer.innerHTML = `<audio controls><source src="${audioSource}.ogg" type="audio/ogg"><source src="${audioSource}.mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>`;
+  const audioSource = block.querySelector('a').href;
+  audioContainer.innerHTML = `<audio controls>
+    <source src="${audioSource}.ogg" type="audio/ogg">
+    <source src="${audioSource}.mp3" type="audio/mpeg">
+  Your browser does not support the audio element.
+  </audio>`;
 }
