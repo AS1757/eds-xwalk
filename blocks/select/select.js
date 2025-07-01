89b7de89ab48e3c396ead082eabe577de298ffa6
Amit K Singh <Amit.Kumar.Singh8@gds.ey.com>
2025-07-01 22:08:28 +0530

    select dropdown with spreadsheet data

--- blocks/select/select.js
+++ blocks/select/select.js
@@ -1,9 +1,29 @@
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Response status: ${error.status}`);
+  }
+}
+
 export default function decorate(block) {
-  const selectElement = `<select name="cars" id="cars">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
-</select>`;
+  const optionsURL = block.children[0].querySelector('a')?.href;
+  const selectElement = document.createElement('select');
+  if (optionsURL) {
+    fetchData(optionsURL).then((optionsData) => {
+      const options = optionsData.data;
+      options.forEach((option) => {
+        const optionElement = document.createElement('option');
+        optionElement.textContent = option.value;
+        optionElement.value = option.key;
+        selectElement.append(optionElement);
+      });
+    });
+  }
   block.append(selectElement);
 }
