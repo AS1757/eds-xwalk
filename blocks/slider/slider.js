92afc85ca406b6166eca7b80d193e510e657f06a
Amit K Singh <Amit.Kumar.Singh8@gds.ey.com>
2025-07-03 21:44:27 +0530

    slider  model

--- blocks/slider/slider.js
+++ blocks/slider/slider.js
@@ -1,17 +1,20 @@
 export default function decorate(block) {
-  const min = block.children[0].textContent.trim() || 1;
+  const min = block.children[0].textContent.trim() || 0;
   const max = block.children[1].textContent.trim() || 10;
   const step = block.children[2].textContent.trim() || 1;
+  const value = block.children[3].textContent.trim() || 5;
   const rangeInput = document.createElement('input');
   const rangeInputValue = document.createElement('p');
   rangeInput.type = 'range';
   rangeInput.min = min;
   rangeInput.max = max;
+  rangeInput.value = value;
   rangeInput.step = step;
 
   block.append(rangeInput);
   block.append(rangeInputValue);
+  rangeInputValue.innerText = rangeInput.value;
   rangeInput.addEventListener('change', (e) => {
-    rangeInputValue.innerText = e.value;
+    rangeInputValue.innerText = e.target.value;
   });
 }
