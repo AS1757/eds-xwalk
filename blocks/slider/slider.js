f3b570629406da21dec3fb0e814d29b649334515
Amit K Singh <Amit.Kumar.Singh8@gds.ey.com>
2025-07-03 21:47:52 +0530

    text number type

--- blocks/slider/slider.js
+++ blocks/slider/slider.js
@@ -2,7 +2,7 @@ export default function decorate(block) {
   const min = block.children[0].textContent.trim() || 0;
   const max = block.children[1].textContent.trim() || 10;
   const step = block.children[2].textContent.trim() || 1;
-  const value = block.children[3].textContent.trim() || 5;
+  const value = block.children[3].textContent.trim() || 6;
   const rangeInput = document.createElement('input');
   const rangeInputValue = document.createElement('p');
   rangeInput.type = 'range';
