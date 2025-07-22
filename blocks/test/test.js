487ff66839ae3dbcd2808446f1be74c6a40367c9
Amit K Singh <Amit.Kumar.Singh8@gds.ey.com>
2025-07-22 17:57:08 +0530

    fix linting issue

--- blocks/test/test.js
+++
@@ -1,7 +0,0 @@
-export default function decorate(block) {
-  const test = block.children[0].textContent.trim();
-  const elStrong = document.createElement('strong');
-  elStrong.innerText = test;
-  elStrong.classList.add('test-class');
-  block.append(elStrong);
-}
