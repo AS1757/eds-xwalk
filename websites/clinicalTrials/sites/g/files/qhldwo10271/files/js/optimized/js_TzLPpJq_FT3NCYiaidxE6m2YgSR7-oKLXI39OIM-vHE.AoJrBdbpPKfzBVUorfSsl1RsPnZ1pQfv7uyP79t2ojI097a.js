cecc1f91c55ed7dce3d62bf6e2c501fa5e856ba0
As1757Otsuka <AmitK.Singh-CW@otsuka-us.com>
2025-12-09 13:53:23 +0530

    cleanup

--- websites/clinicalTrials/sites/g/files/qhldwo10271/files/js/optimized/js_TzLPpJq_FT3NCYiaidxE6m2YgSR7-oKLXI39OIM-vHE.AoJrBdbpPKfzBVUorfSsl1RsPnZ1pQfv7uyP79t2ojI097a.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function () {
-    jQuery('option[value="contains"]').text('By Phrase');
-    jQuery('option[value="allwords"]').text('All Words');
-    jQuery('option[value="word"]').text('Near Words');
-    jQuery('option[value="regular_expression"]').text('Either Word');
-});
